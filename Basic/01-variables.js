3dc728897fd77de5e87984b5923c47b5d737743a
mateomarincana977-sys <mateomarincana977@gmail.com>
2025-11-07 09:04:05 +0100

    Mate edit

--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -11,6 +11,10 @@ console.log(helloWorld)
 helloWorld = "¡Hola de nuevo, JavaScript!"
 console.log(helloWorld)
 
+
+var helloWorld = "Hola, Javascript"
+console.log(helloWorld)
+
 // let
 
 let helloWorld2 = "¡Hola, JavaScript 2!"
@@ -19,10 +23,18 @@ console.log(helloWorld2)
 helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
 console.log(helloWorld2)
 
+let helloMundo = "Hola Javascript 2"
+console.log (helloMundo)
+
+helloMundo = "Hola de nuevo, Javascript 2"
+
 // const
 
 const helloWorld3 = "¡Hola, JavaScript 3!"
 console.log(helloWorld3)
+
+const helloMundo3 = "Hola, Javascript 3"
+console.log (helloMundo3)
 
 // Error
 // helloWorld3 = "¡Hola de nuevo, JavaScript 2!"
